728b57b7899919544216b2ba7682102d5e73b1be
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-10-24 21:49:19 +0300

    init

---
+++ UIKitMovie/UIKitMovie/SceneDelegate.swift
@@ -0,0 +1,15 @@
+// SceneDelegate.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Начальный SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {}
+}
